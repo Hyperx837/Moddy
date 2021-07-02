8a36c3403979b22a3e8aad8d2fd2fe992b6fd279
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-03 05:11:53 +0530

    🎨 added a general cog and added few commands

---
+++ moddy/cogs/general.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from moddy.utils import log
+from discord.ext import commands
+import discord
+
+class General(commands.Cog):
+    """The description for General goes here."""
+
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+    
+    @commands.command(name="clear")
+    @commands.has_permissions(manage_messages=True)
+    async def clear_messages(self, ctx: commands.Context, amount: int = 0):
+        # if ctx.author
+        await ctx.channel.purge(limit=amount)
+    
+    @commands.command(name="reload")
+    @commands.is_owner()
+    async def reload_cogs(self, *_):
+        self.bot.load_cogs(mode="reload")
+        log("[bold green]Cogs successfully reloaded")
+
+
+def setup(bot):
+    bot.add_cog(General(bot))

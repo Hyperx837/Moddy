6e45f6f41319cf4b6aa9ed68eebf1846306bb965
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-03 17:27:27 +0530

    🚸 shows errors messages to end user

--- moddy/cogs/general.py
+++ moddy/cogs/general.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
-from moddy.utils import log
+import asyncio
+
 from discord.ext import commands
-import discord
+from moddy.embeds import command_not_allowed
+from moddy.utils import log
+
 
 class General(commands.Cog):
     """The description for General goes here."""
 
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-    
+
     @commands.command(name="clear")
-    @commands.has_permissions(manage_messages=True)
     async def clear_messages(self, ctx: commands.Context, amount: int = 0):
-        await ctx.channel.purge(limit=amount)
-    
+        if ctx.author.permissions_in(ctx.channel).manage_messages:
+            await ctx.channel.purge(limit=amount)
+
+        else:
+            await ctx.send(embed=command_not_allowed("clear", "Manage messages"))
+
     @commands.command(name="reload")
     @commands.is_owner()
     async def reload_cogs(self, *_):

22fbf0ff6675aec4c8fef3bf585efeb653b6d29a
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-07 14:05:51 +0530

    🔊 added a log message on exit

--- moddy/__main__.py
+++ moddy/__main__.py
@@ -1,5 +1,13 @@
+from moddy.utils import log
 from moddy.bot import main
+import asyncio
+from moddy.utils import session
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        log("Exiting....")
+        asyncio.run(session.close())

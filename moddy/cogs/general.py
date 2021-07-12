d65e4b8f04e37d336b1feb6ce5bda35c7c98e32f
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-12 18:05:33 +0530

    ✨ added ping command

--- moddy/cogs/general.py
+++ moddy/cogs/general.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-import asyncio
+
 
 from discord.ext import commands
-from moddy.embeds import command_not_allowed
-from moddy.utils import log
+from moddy.embeds import ModdyEmbed, command_not_allowed, reload_embed
+from moddy.utils import benchmark, log, reloadr, timer
+
+reloadr()
 
 
 class General(commands.Cog):
@@ -12,6 +14,11 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
+    #  @commands.command(name="clear")
+    #  @commands.is_owner()
+    #  async def clear_messages(self, ctx: commands.Context, amount: int = 0):
+    #     await ctx.channel.purge(limit=amount + 1)
+
     @commands.command(name="clear")
     async def clear_messages(self, ctx: commands.Context, amount: int = 0):
         if ctx.author.permissions_in(ctx.channel).manage_messages:
@@ -19,12 +26,32 @@ class General(commands.Cog):
 
         else:
             await ctx.send(embed=command_not_allowed("clear", "Manage messages"))
+            log(ctx.guild.id)
 
     @commands.command(name="reload")
-    @commands.is_owner()
-    async def reload_cogs(self, *_):
-        self.bot.load_cogs(mode="reload")
-        log("[bold green]Cogs successfully reloaded")
+    async def reload_cogs(self, ctx: commands.Context):
+        print("pong")
+        if await self.bot.is_owner(ctx.author):
+            self.bot.load_cogs(reload=True)
+            await ctx.send(embed=reload_embed)
+            log("[bold green]Cogs successfully reloaded")
+
+        else:
+            await ctx.send(
+                embed=ModdyEmbed(
+                    "Please don't embarass your self",
+                    "this command is limited to my owner",
+                )
+            )
+
+    @commands.command(name="ping")
+    async def ping(self, ctx: commands.Context):
+        """Get the bot's current websocket and API latency."""
+        (message, duration) = timer(ctx.send("Testing Ping..."))
+        print(message)
+        await message.edit(
+            content=f"Pong! {round(self.bot.latency * 1000)}ms\nAPI: {round(duration * 1000)}ms"
+        )
 
 
 def setup(bot):

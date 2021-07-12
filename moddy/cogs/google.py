c02cf04f5e0a18347bf48e191c97369d44d94223
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-12 09:42:46 +0530

    ♻️ splitted code and improved readability

--- moddy/cogs/google.py
+++ moddy/cogs/google.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from functools import lru_cache
-from typing import Tuple, Type
+from typing import Optional, Tuple
 
 from bs4 import BeautifulSoup, Tag
 from discord.ext import commands
@@ -16,11 +16,7 @@ class Google(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @lru_cache(maxsize=50)
-    def scrape_data(self, data) -> Tuple[str, str, str, str]:
-        soup = BeautifulSoup(data, "lxml")
-        answer_selectors = {
+        self.answer_selectors = {
             "div.wDYxhc:nth-child(2) > div.IZ6rdc": "",  # main paragraph
             ".Z0LcW": "",  # highlted result
             ".XDTKBd": "",  # main result
@@ -28,34 +24,45 @@ class Google(commands.Cog):
             ".kno-rdesc > span:nth-child(2)": "",  # sidebar result
             ".VwiC3b": ".yuRUbf > a",  # first result
         }
+        self.img_selectors = {"g-img.ivg-i": "data-lpage"}
 
-        img_selectors = {"g-img.ivg-i": "data-lpage", "#dimg_46": "src"}
+    def get_image(self) -> Optional[str]:
+        for img_selector, source_attr in self.img_selectors.items():
+            img: Tag = self.soup.select_one(img_selector)
+            if img:
+                return img.get(source_attr)
+        return None
 
-        for ans_selector, link_selector in answer_selectors.items():
-            answer: Tag = soup.select_one(ans_selector)
+    def get_answer(self) -> str:
+        for ans_selector, link_selector in self.answer_selectors.items():
+            answer: Tag = self.soup.select_one(ans_selector)
             if answer and answer.text:
                 link: str = (
-                    f'[Read More....]({soup.select_one(link_selector)["href"]})'
+                    f'[Read More....]({self.soup.select_one(link_selector)["href"]})'
                     if link_selector
                     else ""
                 )
                 break
 
-        for img_selector, source_attr in img_selectors.items():
-            img: Tag = soup.select_one(img_selector)
-            if img:
-                img_src = img.get(source_attr)
-                break
+        answer = self.process_answer(answer.text)
+        return f"{answer}\n{link}"
 
-        return answer.text, link, ans_selector, img_src
+    @lru_cache(maxsize=50)
+    def scrape_data(self, data) -> Tuple[str, Optional[str]]:
+        self.soup = BeautifulSoup(data, "lxml")
+        img_src = self.get_image()
+        answer = self.get_answer()
+        return answer, img_src
 
     def process_answer(self, answer: str) -> str:
         if not answer.endswith("..."):
             return answer
 
+        # omit last sentence if it doesn't have more than 8 letters
+        # 8 characters doesn't make a meaningful sentence
         sentences = answer.split(". ")
         if len(sentences[-1]) < 8:
-            sentences = sentences[:-1]  # omit last sentence
+            sentences = sentences[:-1]
 
         return ". ".join(sentences)
 
@@ -66,33 +73,11 @@ class Google(commands.Cog):
             f"https://google.com/search?q={'+'.join(query)}", text=True
         )
         print(data, file=open("text.html", "w"))
-        raw_answer, link, selector, img = self.scrape_data(data)
-        log(
-            get_mention(ctx.author),
-            f'got an answer for question "{" ".join(query)}" with {selector}',
-        )
-        answer = self.process_answer(raw_answer)
-        await ctx.send(
-            embed=google_embed(" ".join(query), f"{answer}\n{link}", img=img)
-        )
 
-    @commands.command("google")
-    async def search(self, ctx: commands.Context, *query):
-        """The main function of the package that puts everything together"""
+        answer, img = self.scrape_data(data)
+        log(get_mention(ctx.author), f'got an answer for question "{" ".join(query)}"')
 
-        data = await get_url(
-            f"https://google.com/search?q={'+'.join(query)}", text=True
-        )
-        print(data, file=open("text.html", "w"))
-        raw_answer, link, selector, _ = self.scrape_data(data)
-        answer = self.process_answer(raw_answer)
-        await ctx.send(
-            embed=google_embed(" ".join(query), f"{answer}\n{link}\n`{selector}`")
-        )
-
-    @search.error
-    async def foo(ctx: commands.Context, error: Type[commands.CommandError]):  # type: ignore[return]
-        log(get_mention(ctx.author), f"Caused the error {error}")
+        await ctx.send(embed=google_embed(" ".join(query), answer, img=img))
 
 
 def setup(bot):

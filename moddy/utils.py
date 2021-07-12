5bbfe995ff9bb3302db73166ae6c6b24e973e066
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-12 18:21:35 +0530

    🔨 added a function to reload files

--- moddy/utils.py
+++ moddy/utils.py
@@ -1,10 +1,32 @@
+import importlib
 from typing import Callable, Union
 
 import aiohttp
 from aiohttp import ClientResponse
 from rich.console import Console
 
-log = Console().log
+log: Callable = Console().log
+
+
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
+
+
+def reloadr():
+    from moddy import embeds, utils
+
+    importlib.reload(utils)
+    importlib.reload(embeds)
 
 
 async def get_url(

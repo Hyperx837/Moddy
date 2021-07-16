eb25cd8667b76eea294e21b32d63240261449146
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-16 11:21:10 +0530

    🩹 fix ping command

--- moddy/cogs/general.py
+++ moddy/cogs/general.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
+import asyncio
+
 from discord.ext import commands
-from moddy.embeds import ModdyEmbed, command_not_allowed, reload_embed
-from moddy.utils import log, reloadr
+from discord.message import Message
+from moddy.embeds import ModdyEmbed, command_not_allowed, ping_embed, reload_embed
+from moddy.utils import benchmark, log, reloadr
 
 reloadr()
-
-
-def timer(arg):
-    pass
 
 
 class General(commands.Cog):
@@ -34,7 +33,6 @@ class General(commands.Cog):
 
     @commands.command(name="reload")
     async def reload_cogs(self, ctx: commands.Context):
-        print("pong")
         if await self.bot.is_owner(ctx.author):
             self.bot.load_cogs(reload=True)
             await ctx.send(embed=reload_embed)
@@ -51,11 +49,10 @@ class General(commands.Cog):
     @commands.command(name="ping")
     async def ping(self, ctx: commands.Context):
         """Get the bot's current websocket and API latency."""
-        (message, duration) = timer(ctx.send("Testing Ping..."))
-        print(message)
-        await message.edit(
-            content=f"Pong! {round(self.bot.latency * 1000)}ms\nAPI: {round(duration * 1000)}ms"
-        )
+        with benchmark() as timer:
+            message: Message = await ctx.send("Testing Ping...")
+        asyncio.create_task(message.delete())
+        await ctx.send(embed=ping_embed(self.bot.latency, timer.elapsed))
 
 
 def setup(bot):

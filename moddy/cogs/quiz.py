a48d3f218f8b9c29b47c221a61d123c0ab1fe4f2
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-19 06:30:06 +0530

    ♻️ moved lot of code to datastructures

--- moddy/cogs/quiz.py
+++ moddy/cogs/quiz.py
@@ -6,12 +6,13 @@ import random
 import discord
 import moddy.bot
 from discord.ext import commands
+from moddy import datastructures
 from moddy.database.get_data import scrape
+from moddy.datastructures import DictStack, Question
 from moddy.embeds import ModdyEmbed, ModdySuccess
 from moddy.utils import benchmark, get_mention, languages, log, numbers, reloadr
-from motor.motor_asyncio import AsyncIOMotorCollection
 
-reloadr()
+reloadr(datastructures)
 
 
 class Quiz(commands.Cog):
@@ -19,58 +20,58 @@ class Quiz(commands.Cog):
 
     def __init__(self, bot):
         self.bot: moddy.bot.DiscordBot = bot
-        self.question = None
-        self.questions: dict = {}
+        self.questions: DictStack = DictStack()
 
-    async def get_question(self, language):
-        quiz_coll: AsyncIOMotorCollection = self.bot.db.quiz  # gets quiz collection
-        lang = language or random.choice(languages)
-        (self.question,) = [
-            doc
-            async for doc in quiz_coll.aggregate(
-                [{"$match": {"language": lang}}, {"$sample": {"size": 1}}]
-            )
-        ]  # get random question from db
-
-    def parse_data(self):
-        ques = self.question
+    def parse_data(self, ques: Question):
         answers = "\n".join(
-            f"{i}. {ans}" for i, ans in enumerate(ques["answers"].values(), 1)
+            f"{i}. {ans}" for i, ans in enumerate(ques.answers.values(), 1)
         )
-        code: str = ques["code"]
+        code: str = ques.code
         if code:
             if code.count("\n") > 1:
                 answers = f"```py\n{code}```\n{answers}"
             else:
                 answers = f"`{code}`\n{answers}"
-        question = f"{ques['title']} ({ques['language']})"  # question with language
+        question = f"{ques.title} ({ques.language})"  # question with language
         return question, answers
 
     @commands.command(name="quiz")
     async def quiz(self, ctx: commands.Context, lang: str = ""):
         log(get_mention(ctx.author), f"Requested a quiz with {ctx.message.content}")
-        await self.get_question(lang)
-        title, answers = self.parse_data()
+        lang = lang or random.choice(languages)
+        (question,) = await self.questions.random_questions({"language": lang}, 1)
+        title, answers = self.parse_data(question)
         msg: discord.Message = await ctx.send(
             embed=ModdyEmbed(title=title, description=answers)
         )
-        self.questions[msg.id] = self.question
+        print(msg.id)
+        await self.questions.setitem(msg.id, question.dict())
         await asyncio.gather(*(msg.add_reaction(num) for num in numbers.values()))
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
-        msg: discord.Message = reaction.message
-        emoji: discord.Emoji = reaction.emoji
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        channel: discord.TextChannel = self.bot.get_channel(payload.channel_id)
+        member: discord.Member = await channel.guild.fetch_member(payload.user_id)
+        msg: discord.Message = await channel.fetch_message(payload.message_id)
+        emoji: str = payload.emoji._as_reaction()
+        self.question: Question = await self.questions.getitem(msg.id)
         # if bot was the one to react to the message or reaction was added to another message
-        if user == self.bot.user or self.bot.user != msg.author:
+        conds = (
+            member == self.bot.user,
+            self.bot.user != msg.author,
+        )
+        if any(conds):
             return
 
+        asyncio.create_task(msg.remove_reaction(emoji, member))
+        if self.question.has_answered(member):
+            return
         log(
-            f'question "{msg.content[:20]}..." was reacted {emoji} by user "{get_mention(user)}'
+            f'question "{msg.embeds[0].title[:20]}..." was reacted {emoji} by user {get_mention(member)}'
         )
         log(f"Guild: {msg.guild} | Channel: {msg.channel}")
-        self.question = self.questions[msg.id]
-        correct_answer = self.question["correct_answer"]
+        self.question.answered_by(member)
+        correct_answer = self.question.correct_answer
         correct_reaction = numbers[correct_answer]
         result = (
             "I knew that you were the only one who was going to get it the first time"
@@ -78,8 +79,7 @@ class Quiz(commands.Cog):
             else "I had told my friend that you might cause a nexus event if you get it right the first time"
         )
 
-        await user.send(result)
-        await msg.remove_reaction(reaction.emoji, user)
+        await member.send(result)
 
     @commands.command("new-quizes")
     @commands.is_owner()

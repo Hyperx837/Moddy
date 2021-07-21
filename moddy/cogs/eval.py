d87335e1438d4e802001b37b159a3bb67520b0e5
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-21 20:17:46 +0530

    ✨ added a new command to evaluate and debug expressions

---
+++ moddy/cogs/eval.py
@@ -0,0 +1,39 @@
+import importlib  # noqa: F401
+from inspect import iscoroutine
+
+import moddy.main  # noqa: F401
+from discord.ext import commands
+import asyncio
+
+
+class Eval(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.task = asyncio.create_task
+
+    @commands.command("exec")
+    @commands.is_owner()
+    async def exec(self, ctx, *expression):
+        out = exec(" ".join(expression))
+        if iscoroutine(out):
+            await out
+
+    @commands.command("eval")
+    @commands.is_owner()
+    async def eval(self, ctx: commands.Context, expression: str):
+        try:
+            output = eval(expression)
+
+        except Exception as exc:
+            output = f"`Error: {exc}`"
+
+        await ctx.send(output)
+
+    def evaluater(self, expression):
+        if "=" in expression:
+            lhs, rhs = expression.split(" = ")
+            globals[lhs] = rhs
+
+
+def setup(bot):
+    bot.add_cog(Eval(bot))

bc30bef0f4c6f212e9e3196b143a0e77bf8e8d9b
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-07-29 05:50:30 +0530

    ⏪ readded get_url

--- moddy/utils.py
+++ moddy/utils.py
@@ -3,12 +3,26 @@ import contextlib
 import importlib
 import time
 from dataclasses import dataclass
-from typing import Coroutine
+from typing import Coroutine, Union
 
 import discord
+from aiohttp import ClientResponse
 from rich.console import Console
 
+import moddy.main
 
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
 event_loop = asyncio.get_event_loop()
 console = Console()
 numbers = {"A": "1️⃣", "B": "2️⃣", "C": "3️⃣", "D": "4️⃣"}
@@ -31,6 +45,22 @@ def call_every(*, secs):
         return schedular
 
     return wrapper
+
+
+async def get_url(
+    url, *args, json=False, text=False, **kwargs
+) -> Union[ClientResponse, dict, str]:
+    session = moddy.main.moddity.http
+    async with session.get(  # type: ignore
+        url, headers=headers, *args, **kwargs
+    ) as response:
+        if json:
+            return await response.json()
+
+        elif text:
+            return await response.text()
+
+        return response
 
 
 @dataclass

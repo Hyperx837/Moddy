cdf81f8ee1bb0fc65c483e8cb4759f84c167c3ea
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2021-09-04 05:11:49 +0530

    🔨 improved cog finding function

--- moddy/bot.py
+++ moddy/bot.py
@@ -1,6 +1,7 @@
 import os
 
 from discord.ext import commands
+from .config import cog_paths
 from discord.ext.commands.errors import ExtensionAlreadyLoaded
 
 from .logger import logger
@@ -15,22 +16,11 @@ class DiscordBot(commands.Bot):
 
     @property
     def cogs(self):
-        cog_files = os.listdir("moddy/cogs/")
-        for file in cog_files:
-            if file.endswith(".py") and not file.startswith("__"):
-                yield f"moddy.cogs.{file.strip('.py')}"
-        # cog_modules = (
-        #     importlib.import_module(f"moddy.cogs.{cog_file.strip('.py')}")
-        #     for cog_file in cog_files
-        #     if cog_file.endswith(".py")
-        #  )
-        # glob_vals = []
-        # for module in cog_modules:
-        #     glob_vals.extend(module.__dict__.values())
-
-        # for val in glob_vals:
-        #     if isinstance(val, commands.CogMeta):
-        #         yield val
+        for path in cog_paths:
+            cog_files = os.listdir(f"moddy/cogs/{path}")
+            for file in cog_files:
+                if file.endswith(".py") and not file.startswith("__"):
+                    yield f"moddy.cogs.{path.replace('/', '.')}{file.strip('.py')}"
 
     def load_cogs(self, *, reload=False):
         loader = self.reload_extension if reload else self.load_extension

d4c8c8aecbea87f630df25911934523b7c5987be
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2021-09-04 21:47:25 +0530

    ✨ added a listener method

--- moddy/limit_guilds.py
+++ moddy/limit_guilds.py
@@ -1,6 +1,8 @@
 import functools
-from typing import Any, Callable, List, TypeVar
+from moddy.bot import DiscordBot
+from typing import Any, Callable, List, Type, TypeVar
 
+from discord import Message
 from discord.ext import commands
 
 import moddy.main
@@ -9,18 +11,23 @@ from moddy.logger import logger
 deco = TypeVar("deco", bound=Callable[..., Any])
 
 
-class LimitedGuilds(commands.Cog):
+class limitedguilds(commands.Cog):
     cmds: List[commands.Command] = []
+    listeners: List[Callable]
 
     def __init__(self, bot) -> None:
-        self.bot: commands.Bot = bot
+        self.bot: DiscordBot = bot
 
-    @classmethod
-    def command(cls, *args, **kwargs):  # type: ignore
+    @property
+    def guild_id(self) -> int:
+        return 0
+
+    @staticmethod
+    def command(*args, **kwargs):  # type: ignore
         def wrapper(func: deco):
             @functools.wraps(func)
             async def comm(self, ctx: commands.Context, *args, **kwargs):
-                guild_id = func.__self__.guild_id  # type: ignore
+                guild_id = self.guild_id  # type: ignore
                 guild = moddy.main.moddity.bot.get_guild(guild_id)
                 if guild != ctx.guild:
                     return
@@ -30,16 +37,20 @@ class LimitedGuilds(commands.Cog):
 
         return wrapper
 
-    # def __getattribute__(self, name: str):
-    #     value = super().__getattribute__(name)
-    #     if isinstance(value, tuple):
-    #         guild_id = super().__getattr__("guild_id")
-    #         attr_val = tuple(
-    #             # commands.Command.__call__
+    @staticmethod
+    def listener(name=""):  # type: ignore
+        def wrapper(func: deco):
+            @functools.wraps(func)
+            async def comm(self: Type[limitedguilds], arg1: Any, *args, **kwargs):
+                guild_id = self.guild_id  # type: ignore
+                required_guild = self.bot.get_guild(guild_id)
 
-    #             for func in value
-    #             if isinstance(func, commands.Command)
-    #         )
-    #         return attr_val
+                if isinstance(arg1, Message):
+                    guild = arg1.guild
+                if required_guild != guild:
+                    return
+                return await func(self, arg1, *args, **kwargs)
 
-    #     return value
+            return commands.Cog.listener(name=name)(comm)
+
+        return wrapper

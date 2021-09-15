0aa8d098267c758064065f551f25a95d1c850f34
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2021-09-15 12:48:35 +0530

    🥅 handle missing perm errors in webhooks

--- moddy/cogs/message_webhooks.py
+++ moddy/cogs/message_webhooks.py
@@ -2,26 +2,36 @@
 from typing import Tuple
 
 import discord
+from discord.errors import Forbidden
 from discord.ext import commands
-from moddy.config import my_guild
-from moddy.limit_guilds import limitedguilds
 from moddy.logger import logger
 from moddy.utils import send_hook
 
 
-class MessageWebhooks(limitedguilds):
+class MessageWebhooks(commands.Cog):
     """The description for Developer goes here."""
+
+    def __init__(self, bot) -> None:
+        self.bot: commands.Bot = bot
 
     @commands.command(name="replicater")
     async def send_webhook(self, ctx: commands.Context, user: discord.Member, *words):
         message = " ".join(words)
-        if ctx.guild.id != my_guild:
+        # if not self.bot.user.permissions_in(ctx.channel).manage_webhooks:
+
+        if (
+            not ctx.author.permissions_in(ctx.channel).manage_webhooks
+            and not self.bot.owner_id == ctx.author.id
+        ):
+            await ctx.send("You don't have manage webhooks permission")
             return
 
-        if not ctx.author.permissions_in(ctx.channel).manage_webhooks:
-            return
+        try:
+            await send_hook("Mod Hook", ctx.channel, message, user=user)
 
-        await send_hook("Hooker", ctx.channel, message, user=user)
+        except Forbidden:
+            await ctx.send("I don't have manage webhooks permission")
+            await ctx.send(":cry:")
 
 
 def setup(bot):

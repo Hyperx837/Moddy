e351c2a03b97f43a73a91814cff6f817a8e800dd
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2021-09-18 21:34:54 +0530

    🥅 catch errors and simlified code

--- moddy/utils/db.py
+++ moddy/utils/db.py
@@ -1,12 +1,14 @@
-
 from motor.motor_asyncio import AsyncIOMotorCollection
 
 
 async def get_doc(coll: AsyncIOMotorCollection, filt: dict):
-    pipeline = [{"match": filt}, {"$sample": {"size": 1}}]
-    return [doc async for doc in coll.aggregate(pipeline)][0]
+    pipeline = [{"$match": filt}, {"$sample": {"size": 1}}]
+    try:
+        return [doc async for doc in coll.aggregate(pipeline)][0]
+
+    except IndexError:
+        return None
 
 
 async def get_random_doc(coll: AsyncIOMotorCollection):
-    pipeline = [{"$sample": {"size": 1}}]
-    return [doc async for doc in coll.aggregate(pipeline)][0]
+    return await get_doc(coll, {})
